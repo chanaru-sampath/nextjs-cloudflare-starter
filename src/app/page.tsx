b7e03c8e99546b5f0dbb69df1dce74ce94831894
chanaru-sampath <chanarusampath@gmail.com>
2025-10-26 19:18:31 +0530

    code refactor

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,7 @@
 export default function Home() {
-  return <div>Nextjs Cloudflare Starter</div>;
+  return (
+    <div className="w-screen h-screen flex justify-center items-center">
+      <div className="font-bold text-blue-600 text-8xl">Nextjs Cloudflare Starter</div>
+    </div>
+  );
 }
